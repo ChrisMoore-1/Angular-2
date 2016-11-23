eb9fed72e25ce21e9e5c0ada54180ec0a063f540
DividedTruth <christohperdmoore75@gmail.com>
2016-11-23 16:31:41 -0700

    first commit

---
+++ 02-direcitves/src/app/highlight.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[dirHighlight]'
+})
+export class HighlightDirective {
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = 'green';
+  };
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  }
+  private backgroundColor = 'white';
+  constructor() {
+    // this.elementRef.nativeElement.style.backgroundColor = 'green';
+    // this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+   }
+
+}
